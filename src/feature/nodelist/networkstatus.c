97c0493d7d7b424f12eac91ae78c74ccbf2efac5
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-10-09 23:05:26 +0530

    lint: fix

--- src/feature/nodelist/networkstatus.c
+++ src/feature/nodelist/networkstatus.c
@@ -859,7 +859,7 @@ router_get_consensus_status_by_id(const char *digest)
  * For certificate fetches, use we_want_to_fetch_unknown_auth_certs, and
  * for serving fetched documents, use directory_caches_dir_info. */
 int
-we_want_to_fetch_flavor (const or_options_t *options, int flavor)
+we_want_to_fetch_flavor(const or_options_t *options, int flavor)
 {
   if (flavor < 0 || flavor > N_CONSENSUS_FLAVORS) {
     /* This flavor is crazy; we don't want it */
